d7747582c87309ff4a5a9c1693af30005dd8ecba
Erik Benson <ebenson@geodecisions.com>
2023-03-17 17:09:10 -0400

    feat: updates redis to 4.6.5 and reworks module to work with new redis api and to align to memory cache layout

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
-  extends: ['standard']
-}
+  env: {
+    commonjs: true,
+    es6: true,
+    mocha: true,
+    jest: true,
+    jasmine: true
+  },
+  extends: 'eslint:recommended',
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    process: 'readonly',
+    console: 'readonly',
+    __dirname: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+  },
+  rules: {
+    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
+    indent: ['error', 2],
+    'linebreak-style': ['error', 'unix'],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+  },
+};
